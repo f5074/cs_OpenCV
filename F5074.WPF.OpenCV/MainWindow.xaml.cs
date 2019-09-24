42b6febdeadef1e10fb542f7df9e16a011984e17
f5074 <oseongryu@gmail.com>
2019-09-24 15:37:29 +0900

    commit

---
+++ F5074.WPF.OpenCV/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+
+
+namespace F5074.WPF.OpenCV
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        CvCapture cap;
+        DispatcherTimer TimerClock;
+        WriteableBitmap wb;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (InitWebCamera())
+            {
+                StartTimer();
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Error");
+            }
+        }
+
+        private bool InitWebCamera()
+        {
+            try
+            {
+                cap = CvCapture.FromCamera(CaptureDevice.DShow, 0); // 0 First Camera
+                cap.FrameWidth = 640;
+                cap.FrameHeight = 480;
+                wb = new WriteableBitmap(cap.FrameWidth, cap.FrameHeight, 96, 96, PixelFormats.Bgr24, null);
+                image.Source = wb;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void StartTimer()
+        {
+            TimerClock = new DispatcherTimer();
+            TimerClock.Interval = new TimeSpan(0, 0, 0, 0, 33);
+            TimerClock.IsEnabled = true;
+            TimerClock.Tick += TimerClock_Tick;
+            
+        }
+
+        private void TimerClock_Tick(object sender, EventArgs e)
+        {
+            using(IplImage src = cap.QueryFrame())
+            {
+                WriteableBitmapConverter.ToWriteableBitmap(src, wb);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            TimerClock.IsEnabled = false;
+            if (cap != null) cap.Dispose();
+        }
+    }
+}
---
+++ F5074.Winforms.OpenCV/Form1.cs
@@ -0,0 +1,56 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.Winforms.OpenCV
+{
+    public partial class Form1 : Form
+    {
+        CvCapture capture;
+        IplImage src;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.FormClosing += Form1_FormClosing;
+
+            // Timer 관련 속성 
+            this.timer1.Tick += Timer1_Tick;
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                capture = CvCapture.FromCamera(CaptureDevice.DShow, 0);
+                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 640);
+                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 480);
+            }
+            catch
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            src = capture.QueryFrame();
+            pictureBoxIpl1.ImageIpl = src;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cv.ReleaseImage(src);
+            if (src != null) src.Dispose();
+        }
+    }
+}
